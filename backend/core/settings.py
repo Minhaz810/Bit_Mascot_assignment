e94cf9d8892ee88f814722e5131790b7f8874bfe
minhaz810 <chowdhuryminhaz021@gmail.com>
2024-10-28 23:02:37 +0600

    cors added

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'medicine_list',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -43,9 +44,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
