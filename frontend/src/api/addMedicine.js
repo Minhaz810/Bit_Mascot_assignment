cacef172b9f2497b39ff19920b312aa6806a6b50
minhaz810 <chowdhuryminhaz021@gmail.com>
2024-10-30 03:41:46 +0600

    all flow done

---
+++ frontend/src/api/addMedicine.js
@@ -0,0 +1,30 @@
+import BASE_URL from "./init";
+
+export const addMedicine = async (payLoad) => {
+    const url = `${BASE_URL}/api/v1/admin/medicine_list`;
+    const token = JSON.parse(localStorage.getItem('userToken')).access;
+
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(payLoad),
+    });
+
+    let result = await response.json();
+
+    if (!response.ok) {
+        result = {
+            "status": response.status,
+            "message": result.detail,
+        };
+    } else {
+        result = {
+            "status": "success",
+            "result": result,
+        };
+    }
+    return result;
+};
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -5,6 +5,7 @@ import { extractPageNumber } from "../api/utils";
 import { useNavigate } from "react-router-dom";
 import { updateMedicine } from "../api/updateMedicine";
 import { deleteMedicine } from "../api/deleteMedicine";
+import { addMedicine } from "../api/addMedicine";
 
 const AdminPage = () => {
     const [medicineList,setMedicineList] = useState([])
@@ -14,7 +15,8 @@ const AdminPage = () => {
     const [prev,setPrev] = useState(null)
     const [query,setQuery] = useState("")
     const [deleteModalOpen,setDeleteModalOpen] = useState(false)
-    const [editModelOpen,setEditModalOpen] = useState(false)
+    const [editModalOpen,setEditModalOpen] = useState(false)
+    const [addModalOpen,setAddModalOpen] = useState(false)
     const [editableData,setEditableData] = useState({})
     const [medicineName, setMedicineName] = useState(editableData?.name || "");
     const [medicinePrice, setMedicinePrice] = useState(editableData?.price || "");
@@ -74,6 +76,11 @@ const AdminPage = () => {
         setEditModalOpen(true)
     }
 
+    const handleAddModalPopUp = () => {
+        setManufacturer(manufacturerList[0])
+        setAddModalOpen(true)
+    }
+
     const handleDelete = async () =>{
         const result = await deleteMedicine(selectedID)
         if (result["status"]==="success"){
@@ -97,6 +104,22 @@ const AdminPage = () => {
         const result = await updateMedicine(payLoad)
         if (result["status"]==="success"){
             setEditModalOpen(false)
+            fetchMedicineList()
+        }
+    }
+    const handleAdd = async () =>{
+        const payLoad = {
+            "name": medicineName,
+            "price": medicinePrice,
+            "manufacturer": manufacturer,
+            "generic_name": genericName,
+            "description": description,
+            "batch_number": batchNumber,
+            "other_related_detailes": otherDetails
+          };
+        const result = await addMedicine(payLoad)
+        if (result["status"]==="success"){
+            setAddModalOpen(false)
             fetchMedicineList()
         }
     }
@@ -139,11 +162,36 @@ const AdminPage = () => {
                 </div>
             </div>
             
-            <div className="flex flex-col mt-4">
-                <div className="flex items-center justify-center">
-                    <h1 className="text-4xl font-bold text-gray-800 ">
-                        Admin Panel
-                    </h1>
+            <div className="flex flex-col mt-4 gap">
+
+                <div className="flex justify-center">
+                    <div className="w-3/4 flex justify-between items-center py-4"> {/* Added padding for spacing */}
+                        <h1 className="text-4xl font-bold text-gray-800 flex-grow text-center">
+                            Admin Panel
+                        </h1>
+                        <button className="bg-blue-500 text-white 
+                        font-semibold py-2 px-4 rounded shadow hover:bg-blue-600 
+                        transition duration-200 flex items-center"
+                        onClick={()=>handleAddModalPopUp()}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="w-5 h-5 mr-2" // Adjust size and margin
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M12 4v16m8-8H4"
+                                />
+                            </svg>
+                            Add Medicine
+                        </button>
+                    </div>
                 </div>
 
                 <div className="flex items-center justify-center mt-8 gap-1">
@@ -248,7 +296,7 @@ const AdminPage = () => {
                 </div>
             )}
 
-            {editModelOpen && (
+            {editModalOpen && (
                 <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
                 <div className="w-full max-w-sm rounded-xl bg-white px-8 py-12 shadow-lg">
                     <div className="mb-6 text-center font-semibold text-lg">
@@ -323,6 +371,83 @@ const AdminPage = () => {
                     <button
                         className="rounded-full bg-[rgba(255,0,0,0.8)] px-10 py-2 font-semibold text-white transition-transform duration-300 hover:scale-105"
                         onClick={() => setEditModalOpen(false)}
+                    >
+                        Cancel
+                    </button>
+                    </div>
+                </div>
+              </div>
+            )}
+
+            {addModalOpen && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                <div className="w-full max-w-sm rounded-xl bg-white px-8 py-12 shadow-lg">
+                    <div className="mb-6 text-center font-semibold text-lg">
+                    Add New Medicine
+                    </div>
+                    <div className="space-y-4">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        onChange={(e) => setMedicineName(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                        type="number"
+                        placeholder="Price"
+                        onChange={(e) => setMedicinePrice(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <select
+                            value=""
+                            onChange={(e) => {
+                            const selectedManufacturer = manufacturerList.find(item => item.id === parseInt(e.target.value));
+                            setManufacturer(selectedManufacturer);
+                        }}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    >
+                        <option value="" disabled>{manufacturer?.name}</option>
+                        {manufacturerList.map((item) => (
+                        <option key={item.id} value={item.id}>
+                            {item.name}
+                        </option>
+                        ))}
+                    </select>
+                    <input
+                        type="text"
+                        placeholder="Generic Name"
+                        onChange={(e) => setGenericName(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        onChange={(e) => setDescription(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Batch Number"
+                        onChange={(e) => setBatchNumber(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                        type="text"
+                        placeholder="Other Details"
+                        onChange={(e) => setOtherDetails(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    </div>
+                    <div className="flex justify-center space-x-4 mt-6">
+                    <button
+                        className="rounded-full bg-[rgba(0,200,0,0.8)] px-10 py-2 font-semibold text-white transition-transform duration-300 hover:scale-105"
+                        onClick={() => handleAdd()}
+                    >
+                        Add
+                    </button>
+                    <button
+                        className="rounded-full bg-[rgba(255,0,0,0.8)] px-10 py-2 font-semibold text-white transition-transform duration-300 hover:scale-105"
+                        onClick={() => setAddModalOpen(false)}
                     >
                         Cancel
                     </button>
